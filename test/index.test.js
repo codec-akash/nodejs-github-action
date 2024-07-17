3e79426c936ec96ec4b1e4ccced15aa69d2ec7c8
codec-akash <akashrdubey@yahoo.in>
2024-07-18 00:49:16 +0530

    test updated

--- test/index.test.js
+++ test/index.test.js
@@ -1,6 +1,6 @@
 const { test, expect } = require('@jest/globals');
 const add = require('../src/index');
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(add(1, 2)).toBe(3);
+test('adds 2 + 2 to equal 4', () => {
+    expect(add(2, 2)).toBe(4);
 });
