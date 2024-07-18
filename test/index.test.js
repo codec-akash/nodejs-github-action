ae2ca664be62e369289af9f9416099df7a3de56f
codec-akash <akashrdubey@yahoo.in>
2024-07-18 15:24:29 +0530

    testing github action

--- test/index.test.js
+++ test/index.test.js
@@ -2,5 +2,5 @@ const { test, expect } = require('@jest/globals');
 const add = require('../src/index');
 
 test('adds 2 + 2 to equal 4', () => {
-    expect(add(2, 2)).toBe(4);
+    expect(add(3, 2)).toBe(5);
 });
